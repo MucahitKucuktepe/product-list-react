f368a968706ff4e07b4f7f6e761c38e69412244a
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-12 16:59:07 +0300

    search input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Categories from "./components/Products/Categories";
+import ProductList from "./components/Products/ProductList";
+import Search from "./components/Products/Search";
+import React, { useState } from "react";
 
-import './App.css';
-import Header from './components/Header/Header';
 
 function App() {
+   const [search, setSearch] = useState("");
+   const handleSearch = (e) => {
+     setSearch(e)
+   };
   return (
     <div>
       <Header />
+      <Categories />
+      <Search handleSearch={handleSearch} />
+      <ProductList search={search} />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { categories } from "../../helper/data";
-import "./Header.scss"
+import "./Header.scss";
 
 const Header = () => {
   console.log(categories);
   return (
     <div className="title">
       <h1>Products List</h1>
-      {categories.map((item) => (
-        <div className="btn-item" key={item}>
-          <button>{item}</button>
-        </div>
-      ))}
     </div>
   );
 };
